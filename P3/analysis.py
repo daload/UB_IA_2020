14f3092407670822b993755648fb3689accbaf42
daload <david.l.adell11@gmail.com>
2020-12-17 16:19:58 +0100

    P3 + Enunciados

---
+++ P3/analysis.py
@@ -0,0 +1,80 @@
+# analysis.py
+# -----------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+######################
+# ANALYSIS QUESTIONS #
+######################
+
+# Set the given parameters to obtain the specified policies through
+# value iteration.
+
+def question2():
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    return answerDiscount, answerNoise
+
+def question3a():
+    answerDiscount = None
+    answerNoise = None
+    answerLivingReward = None
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3b():
+    answerDiscount = None
+    answerNoise = None
+    answerLivingReward = None
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3c():
+    answerDiscount = None
+    answerNoise = None
+    answerLivingReward = None
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3d():
+    answerDiscount = None
+    answerNoise = None
+    answerLivingReward = None
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3e():
+    answerDiscount = None
+    answerNoise = None
+    answerLivingReward = None
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question8():
+    answerEpsilon = 0.5
+    answerLearningRate = 1
+    return 'NOT POSSIBLE'
+    """
+    No és possible, ja que l'algorisme no pot trobar una política suficientment bona en 50 iteracions.
+    Amb èpsilons baixes, el QValue serà gairebé sempre el mateix, ja que és molt poc probable que és
+    faig una acció random. Per l'altra banda, amb una èpsilon alta, és molt difícil, si és que
+    no és impossible, trobar el resultat a base d'accions aleatòries.
+    """
+    return answerEpsilon, answerLearningRate
+    # If not possible, return 'NOT POSSIBLE'
+
+if __name__ == '__main__':
+    print('Answers to analysis questions:')
+    import analysis
+    for q in [q for q in dir(analysis) if q.startswith('question')]:
+        response = getattr(analysis, q)()
+        print('  Question %s:\t%s' % (q, str(response)))
